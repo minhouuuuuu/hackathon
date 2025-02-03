9820cea4be741286341a506648d32160cf5caaed
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2025-02-03 18:59:00 +0100

    ajout component Header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,35 +1,46 @@
 "use client";
 
 import Link from "next/link";
-import Image from "next/image";
-import { useScrollDirection } from "@/hooks/useScrollDirection";
+import { ShimmerButton } from '@/components/ui/shimmer-button';
 
 export default function Header() {
-  const scrollDirection = useScrollDirection();
 
   return (
-    <header
-      className={`fixed z-[100] top-4 w-full px-80 transition-transform duration-300
-        ${scrollDirection === "down" ? "-translate-y-32" : "translate-y-0"}`}
-    >
-      <div className="flex justify-between items-center bg-white text-black rounded-full px-8 py-4">
-        <h2>Logo</h2>
+    <header className="fixed z-[100] top-4 w-full px-20">
+      <div className="flex justify-between items-center bg-white text-black rounded-full p-1">
+        <Link href="/">
+          <ShimmerButton>
+            <h2>Logo</h2>
+          </ShimmerButton>
+        </Link>
+
         <div>
-          <ul className="flex gap-8 items-center">
+          <ul className="flex gap-8 items-center text-xl uppercase --font-geist-sans">
             <li>
-              <Link href="/" className="hover:opacity-70 transition-opacity">Home</Link>
+              <Link href="/equipe" className="hover:text-amber-500 transition">L'équipe</Link>
             </li>
             <li>
-              <Link href="/" className="hover:opacity-70 transition-opacity">Home</Link>
+              <Link href="/resultats" className="hover:text-amber-500 transition">Résultat</Link>
             </li>
             <li>
-              <Link href="/" className="hover:opacity-70 transition-opacity">Home</Link>
+              <Link href="/formation" className="hover:text-amber-500 transition">Formation</Link>
             </li>
-            <button className="hover:opacity-70 transition-opacity">
-              <Image src="/assets/search.svg" alt="search" width={20} height={20} />
-            </button>
+            <li>
+              <Link href="/boutique" className="hover:text-amber-500 transition">Boutique</Link>
+            </li>
+            <li>
+              <Link href="/partenaires" className="hover:text-amber-500 transition">Partenaires</Link>
+            </li>
+            <li>
+              <Link href="/billetterie" className="hover:text-amber-500 transition">Billetterie</Link>
+            </li>
+            <li>
+              <Link href="/infos" className="hover:text-amber-500 transition">Infos</Link>
+            </li>
           </ul>
         </div>
+
+        <a className="bg-black text-xl text-white px-8 py-3 rounded-full hover:text-amber-500 transition" href="#">Se connecter</a>
       </div>
     </header>
   );
