d92dedefd201a3cd8c0c8d3e3c02038c709c2e1b
Aurélie R <aurelie.runser@edu.univ-fcomte.fr>
2025-02-04 10:33:21 +0100

    ajout page user

--- components/Header.jsx
+++ components/Header.jsx
@@ -45,9 +45,9 @@ export default function Header() {
           <button className="hover:opacity-70 transition-opacity">
             <ShoppingCart size={24} className="text-black" />
           </button>
-          <button className="hover:opacity-70 transition-opacity">
+          <Link href="/user" className="hover:opacity-70 transition-opacity">
             <User size={24} className="text-black" />
-          </button>
+          </Link>
           <Link href="/contact" className="bg-black text-xl text-white px-8 py-3 rounded-full hover:opacity-70 transition-opacity">
             Contact
           </Link>
